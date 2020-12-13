d0398792c44d74d860e091f300f0007cda8f0f49
alukard <alukard6942@github.com>
2020-12-13 23:14:47 +0100

    copirite

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer (){
   return (
 	<footer className="bg-dark text-white mt-5 p-4 text-center">
-		Copyright &copy; {new Date().getFullYear()} SpoonSprint
+		Copyright &copy; {new Date().getFullYear()} Kucharka
   	</footer>
   )
 }
